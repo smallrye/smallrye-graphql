5067ea54066042651bd1f79a14c2ff09cd82b574
Phillip Kruger <phillip.kruger@gmail.com>
2019-07-11 10:18:13 +0200

    Removed the src from spqr and rather do a checkout into target. Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- graphql-spqr/src/_test/java/io/leangen/graphql/domain/Character.java
+++
@@ -1,9 +0,0 @@
-package io.leangen.graphql.domain;
-
-import io.leangen.graphql.annotations.types.GraphQLUnion;
-
-@GraphQLUnion(name = "Character", possibleTypeAutoDiscovery = true)
-public interface Character {
-
-    String getName();
-}
