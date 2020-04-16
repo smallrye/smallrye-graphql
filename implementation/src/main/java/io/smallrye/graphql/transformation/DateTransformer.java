c66c96bc2169405e0399cee7f4f84a433b012849
Phillip Kruger <phillip.kruger@gmail.com>
2020-04-16 20:26:59 +0200

    JDK8 fix Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- implementation/src/main/java/io/smallrye/graphql/transformation/DateTransformer.java
+++ implementation/src/main/java/io/smallrye/graphql/transformation/DateTransformer.java
@@ -57,76 +57,76 @@ public class DateTransformer {
 
         // Date
         if (typeClassName.equals(LocalDate.class.getName())) {
-            if (dateTimeFormatter.isEmpty()) {
-                return Optional.of(LocalDate.parse(input));
-            } else {
+            if (dateTimeFormatter.isPresent()) {
                 return Optional.of(LocalDate.parse(input, dateTimeFormatter.get()));
+            } else {
+                return Optional.of(LocalDate.parse(input));
             }
         } else if (typeClassName.equals(java.sql.Date.class.getName())) {
-            if (dateTimeFormatter.isEmpty()) {
-                LocalDate localdate = LocalDate.parse(input);
+            if (dateTimeFormatter.isPresent()) {
+                LocalDate localdate = LocalDate.parse(input, dateTimeFormatter.get());
                 return Optional.of(java.sql.Date.valueOf(localdate));
             } else {
-                LocalDate localdate = LocalDate.parse(input, dateTimeFormatter.get());
+                LocalDate localdate = LocalDate.parse(input);
                 return Optional.of(java.sql.Date.valueOf(localdate));
             }
 
             // Time    
         } else if (typeClassName.equals(LocalTime.class.getName())) {
-            if (dateTimeFormatter.isEmpty()) {
-                return Optional.of(LocalTime.parse(input));
-            } else {
+            if (dateTimeFormatter.isPresent()) {
                 return Optional.of(LocalTime.parse(input, dateTimeFormatter.get()));
+            } else {
+                return Optional.of(LocalTime.parse(input));
             }
         } else if (typeClassName.equals(java.sql.Time.class.getName())) {
-            if (dateTimeFormatter.isEmpty()) {
-                LocalTime localtime = LocalTime.parse(input);
+            if (dateTimeFormatter.isPresent()) {
+                LocalTime localtime = LocalTime.parse(input, dateTimeFormatter.get());
                 return Optional.of(java.sql.Time.valueOf(localtime));
             } else {
-                LocalTime localtime = LocalTime.parse(input, dateTimeFormatter.get());
+                LocalTime localtime = LocalTime.parse(input);
                 return Optional.of(java.sql.Time.valueOf(localtime));
             }
         } else if (typeClassName.equals(OffsetTime.class.getName())) {
-            if (dateTimeFormatter.isEmpty()) {
-                return Optional.of(OffsetTime.parse(input));
-            } else {
+            if (dateTimeFormatter.isPresent()) {
                 return Optional.of(OffsetTime.parse(input, dateTimeFormatter.get()));
+            } else {
+                return Optional.of(OffsetTime.parse(input));
             }
 
             // DateTime    
         } else if (typeClassName.equals(LocalDateTime.class.getName())) {
-            if (dateTimeFormatter.isEmpty()) {
-                return Optional.of(LocalDateTime.parse(input));
-            } else {
+            if (dateTimeFormatter.isPresent()) {
                 return Optional.of(LocalDateTime.parse(input, dateTimeFormatter.get()));
+            } else {
+                return Optional.of(LocalDateTime.parse(input));
             }
         } else if (typeClassName.equals(java.sql.Timestamp.class.getName())) {
-            if (dateTimeFormatter.isEmpty()) {
-                LocalDateTime localdatetime = LocalDateTime.parse(input);
+            if (dateTimeFormatter.isPresent()) {
+                LocalDateTime localdatetime = LocalDateTime.parse(input, dateTimeFormatter.get());
                 return Optional.of(java.sql.Timestamp.valueOf(localdatetime));
             } else {
-                LocalDateTime localdatetime = LocalDateTime.parse(input, dateTimeFormatter.get());
+                LocalDateTime localdatetime = LocalDateTime.parse(input);
                 return Optional.of(java.sql.Timestamp.valueOf(localdatetime));
             }
         } else if (typeClassName.equals(Date.class.getName())) {
-            if (dateTimeFormatter.isEmpty()) {
-                LocalDateTime localdatetime = LocalDateTime.parse(input);
+            if (dateTimeFormatter.isPresent()) {
+                LocalDateTime localdatetime = LocalDateTime.parse(input, dateTimeFormatter.get());
                 return Optional.of(java.util.Date.from(localdatetime.atZone(ZoneId.systemDefault()).toInstant()));
             } else {
-                LocalDateTime localdatetime = LocalDateTime.parse(input, dateTimeFormatter.get());
+                LocalDateTime localdatetime = LocalDateTime.parse(input);
                 return Optional.of(java.util.Date.from(localdatetime.atZone(ZoneId.systemDefault()).toInstant()));
             }
         } else if (typeClassName.equals(ZonedDateTime.class.getName())) {
-            if (dateTimeFormatter.isEmpty()) {
-                return Optional.of(ZonedDateTime.parse(input));
-            } else {
+            if (dateTimeFormatter.isPresent()) {
                 return Optional.of(ZonedDateTime.parse(input, dateTimeFormatter.get()));
+            } else {
+                return Optional.of(ZonedDateTime.parse(input));
             }
         } else if (typeClassName.equals(OffsetDateTime.class.getName())) {
-            if (dateTimeFormatter.isEmpty()) {
-                return Optional.of(OffsetDateTime.parse(input));
-            } else {
+            if (dateTimeFormatter.isPresent()) {
                 return Optional.of(OffsetDateTime.parse(input, dateTimeFormatter.get()));
+            } else {
+                return Optional.of(OffsetDateTime.parse(input));
             }
         } else {
 
