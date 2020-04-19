10e12864c96f7cd3fc1a56899f1ad333b7f7158b
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2020-04-19 14:11:56 +0200

    Reuse transformers
    Signed-off-by: Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>

--- implementation/src/main/java/io/smallrye/graphql/transformation/Transformer.java
+++ implementation/src/main/java/io/smallrye/graphql/transformation/Transformer.java
@@ -10,6 +10,11 @@ import io.smallrye.graphql.schema.model.TransformInfo;
  */
 public interface Transformer {
 
+    PassThroughTransformer PASS_THROUGH_TRANSFORMER = new PassThroughTransformer();
+    UuidTransformer UUID_TRANSFORMER = new UuidTransformer();
+    UrlTransformer URL_TRANSFORMER = new UrlTransformer();
+    UriTransformer URI_TRANSFORMER = new UriTransformer();
+
     static Transformer transformer(Field field) {
         if (field.getTransformInfo().isPresent()) {
             TransformInfo format = field.getTransformInfo().get();
@@ -27,13 +32,13 @@ public interface Transformer {
             }
 
         } else if (Classes.isUUID(field.getReference().getClassName())) {
-            return new UuidTransformer();
+            return UUID_TRANSFORMER;
         } else if (Classes.isURL(field.getReference().getClassName())) {
-            return new UrlTransformer();
+            return URL_TRANSFORMER;
         } else if (Classes.isURI(field.getReference().getClassName())) {
-            return new UriTransformer();
+            return URI_TRANSFORMER;
         }
-        return new PassThroughTransformer();
+        return PASS_THROUGH_TRANSFORMER;
     }
 
     Object in(Object o) throws Exception;
