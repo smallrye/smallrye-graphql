b3e11865e2f1941a61c255f91ab1c93ccb1ed20c
Rüdiger zu Dohna <snackbox@sinntr.eu>
2020-07-17 04:53:33 +0200

    fix #320: null values in mutation

--- client/implementation/src/test/java/test/unit/MutationBehavior.java
+++ client/implementation/src/test/java/test/unit/MutationBehavior.java
@@ -1,7 +1,10 @@
 package test.unit;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.BDDAssertions.then;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Objects;
 
 import org.eclipse.microprofile.graphql.Mutation;
@@ -26,6 +29,17 @@ public class MutationBehavior {
         String greeting = api.createSome("input");
 
         then(fixture.mutation()).isEqualTo("createSome(thing: 'input')");
+        then(greeting).isEqualTo("output");
+    }
+
+    @Test
+    public void shouldCallNullStringMutation() {
+        fixture.returnsData("'createSome':'output'");
+        StringMutationApi api = fixture.builder().build(StringMutationApi.class);
+
+        String greeting = api.createSome(null);
+
+        then(fixture.mutation()).isEqualTo("createSome(thing: null)");
         then(greeting).isEqualTo("output");
     }
 
@@ -72,6 +86,103 @@ public class MutationBehavior {
         Greeting greeting = api.say(new Greeting("hi", 5));
 
         then(fixture.mutation()).isEqualTo("say(greet: {text: 'hi', count: 5}) {text count}");
+        then(greeting).isEqualTo(new Greeting("ho", 3));
+    }
+
+    @Test
+    public void shouldCallGreetingMutationWithNullValue() {
+        fixture.returnsData("'say':{'text':'ho','count':3}");
+        GreetingMutationApi api = fixture.builder().build(GreetingMutationApi.class);
+
+        Greeting greeting = api.say(new Greeting(null, 5));
+
+        then(fixture.mutation()).isEqualTo("say(greet: {text: null, count: 5}) {text count}");
+        then(greeting).isEqualTo(new Greeting("ho", 3));
+    }
+
+    @GraphQlClientApi
+    interface GreetingListMutationApi {
+        @Mutation
+        Greeting say(List<Greeting> greets);
+    }
+
+    @Test
+    public void shouldCallMutationWithListWithNullValue() {
+        fixture.returnsData("'say':{'text':'ho','count':3}");
+        GreetingListMutationApi api = fixture.builder().build(GreetingListMutationApi.class);
+
+        Greeting greeting = api.say(asList(
+                new Greeting("one", 5),
+                null,
+                new Greeting("three", 5)));
+
+        then(fixture.mutation()).isEqualTo("say(greets: [" +
+                "{text: 'one', count: 5}, " +
+                "null, " +
+                "{text: 'three', count: 5}" +
+                "]) {text count}");
+        then(greeting).isEqualTo(new Greeting("ho", 3));
+    }
+
+    private static class GreetingContainer {
+        Greeting greeting;
+        LocalDateTime when;
+
+        @SuppressWarnings("unused")
+        GreetingContainer() {
+        }
+
+        GreetingContainer(Greeting greeting, LocalDateTime when) {
+            this.greeting = greeting;
+            this.when = when;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            GreetingContainer that = (GreetingContainer) o;
+            return Objects.equals(greeting, that.greeting) &&
+                    Objects.equals(when, that.when);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(greeting, when);
+        }
+    }
+
+    @GraphQlClientApi
+    interface NestedGreetingMutationApi {
+        @Mutation
+        Greeting say(GreetingContainer greeting);
+    }
+
+    @Test
+    public void shouldCallMutationWithNestedValue() {
+        fixture.returnsData("'say':{'text':'ho','count':3}");
+        NestedGreetingMutationApi api = fixture.builder().build(NestedGreetingMutationApi.class);
+        LocalDateTime now = LocalDateTime.now();
+
+        Greeting greeting = api.say(new GreetingContainer(new Greeting("one", 5), now));
+
+        then(fixture.mutation()).isEqualTo("say(greeting: {" +
+                "greeting: {text: 'one', count: 5}, " +
+                "when: '" + now + "'}) {text count}");
+        then(greeting).isEqualTo(new Greeting("ho", 3));
+    }
+
+    @Test
+    public void shouldCallMutationWithNestedNullValue() {
+        fixture.returnsData("'say':{'text':'ho','count':3}");
+        NestedGreetingMutationApi api = fixture.builder().build(NestedGreetingMutationApi.class);
+
+        Greeting greeting = api.say(new GreetingContainer(new Greeting(null, 5), null));
+
+        then(fixture.mutation())
+                .isEqualTo("say(greeting: {greeting: {text: null, count: 5}, when: null}) {text count}");
         then(greeting).isEqualTo(new Greeting("ho", 3));
     }
 }
