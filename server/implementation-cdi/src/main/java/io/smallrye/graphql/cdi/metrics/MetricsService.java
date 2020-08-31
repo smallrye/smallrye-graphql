9541a080b77db8667dea5f2a4007f42249fed92e
Phillip Kruger <phillip.kruger@gmail.com>
2020-08-31 14:12:12 +0200

    Metrics change Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

--- server/implementation-cdi/src/main/java/io/smallrye/graphql/cdi/metrics/MetricsService.java
+++ server/implementation-cdi/src/main/java/io/smallrye/graphql/cdi/metrics/MetricsService.java
@@ -11,7 +11,6 @@ import javax.enterprise.util.AnnotationLiteral;
 import org.eclipse.microprofile.metrics.Metadata;
 import org.eclipse.microprofile.metrics.MetricRegistry;
 import org.eclipse.microprofile.metrics.MetricType;
-import org.eclipse.microprofile.metrics.Tag;
 import org.eclipse.microprofile.metrics.annotation.RegistryType;
 
 import io.smallrye.graphql.api.Context;
@@ -28,9 +27,8 @@ import io.smallrye.graphql.spi.EventingService;
 public class MetricsService implements EventingService {
 
     private final MetricRegistry metricRegistry;
+
     private final Map<Context, Long> startTimes = Collections.synchronizedMap(new IdentityHashMap<>());
-    private static final String METRIC_NAME = "mp_graphql";
-    private final String DESCRIPTION = "Call statistics for the operation denoted by the 'name' tag";
 
     public MetricsService() {
         this.metricRegistry = CDI.current().select(MetricRegistry.class, new VendorType()).get();
@@ -38,14 +36,15 @@ public class MetricsService implements EventingService {
 
     @Override
     public Operation createOperation(Operation operation) {
-        final Tag[] tags = getTags(operation);
+        final String name = getName(operation);
+        final String description = getDescription(operation);
 
         Metadata metadata = Metadata.builder()
-                .withName(METRIC_NAME)
+                .withName(name)
                 .withType(MetricType.SIMPLE_TIMER)
-                .withDescription(DESCRIPTION)
+                .withDescription(description)
                 .build();
-        metricRegistry.simpleTimer(metadata, tags);
+        metricRegistry.simpleTimer(metadata);
         return operation;
     }
 
@@ -59,7 +58,7 @@ public class MetricsService implements EventingService {
         Long startTime = startTimes.remove(context);
         if (startTime != null) {
             long duration = System.nanoTime() - startTime;
-            metricRegistry.simpleTimer(METRIC_NAME, getTags(context))
+            metricRegistry.simpleTimer(getName(context))
                     .update(Duration.ofNanos(duration));
         }
     }
@@ -69,19 +68,24 @@ public class MetricsService implements EventingService {
         return ConfigKey.ENABLE_METRICS;
     }
 
-    private Tag[] getTags(Context context) {
-        return new Tag[] {
-                new Tag("name", context.getFieldName()),
-                new Tag("type", context.getOperationType().toString())
-        };
+    private String getName(Context context) {
+        return PRE + context.getOperationType().toString() + UNDERSCORE + context.getFieldName();
     }
 
-    private Tag[] getTags(Operation operation) {
-        return new Tag[] {
-                new Tag("name", operation.getName()),
-                new Tag("type", operation.getOperationType().toString())
-        };
+    private String getName(Operation operation) {
+        return PRE + operation.getOperationType().toString() + UNDERSCORE + operation.getName();
     }
+
+    private String getDescription(Operation operation) {
+        return "Call statistics for the "
+                + operation.getOperationType().toString().toLowerCase()
+                + " '"
+                + operation.getName()
+                + "'";
+    }
+
+    private static final String PRE = "mp_graphql_";
+    private static final String UNDERSCORE = "_";
 
     class VendorType extends AnnotationLiteral<RegistryType> implements RegistryType {
         @Override
