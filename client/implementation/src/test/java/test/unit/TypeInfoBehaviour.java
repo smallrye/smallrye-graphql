e5980ae0484cfac6d4cd8c5290fddcc6b0b1b7f7
Erik Hoogeveen <erik@contactonboard.com>
2020-10-08 08:51:02 +0200

    Prevent ArrayIndexOutOfBound and make it more generally correct

--- client/implementation/src/test/java/test/unit/TypeInfoBehaviour.java
+++ client/implementation/src/test/java/test/unit/TypeInfoBehaviour.java
@@ -2,24 +2,26 @@ package test.unit;
 
 import static org.assertj.core.api.BDDAssertions.then;
 
-import io.smallrye.graphql.client.typesafe.impl.reflection.TypeInfo;
 import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import io.smallrye.graphql.client.typesafe.impl.reflection.TypeInfo;
 
 class TypeInfoBehaviour {
 
-  class Thing {
-    List<OtherThing> things;
-  }
+    class Thing {
+        List<OtherThing> things;
+    }
 
-  class OtherThing {
-    String someValue;
-  }
+    class OtherThing {
+        String someValue;
+    }
 
-  @Test
-  void nullableListField() throws Exception {
-    TypeInfo foo = TypeInfo.of(Thing.class);
-    TypeInfo thingsField = foo.fields().findFirst().get().getType().getItemType();
-    then(thingsField.isNonNull()).isFalse();
-  }
+    @Test
+    void nullableListField() throws Exception {
+        TypeInfo foo = TypeInfo.of(Thing.class);
+        TypeInfo thingsField = foo.fields().findFirst().get().getType().getItemType();
+        then(thingsField.isNonNull()).isFalse();
+    }
 }
