4059afebbac49afdead7efffcb43a9fc56c3e97e
Phillip Kruger <phillip.kruger@gmail.com>
2021-01-26 10:44:31 +0200

    Added test app for (upcoming) batch exception fix. Signed-off-by:Phillip Kruger <phillip.kruger@gmail.com>

---
+++ server/tck/src/test/java/io/smallrye/graphql/test/apps/batch/api/BusinessException.java
@@ -0,0 +1,24 @@
+package io.smallrye.graphql.test.apps.batch.api;
+
+public class BusinessException extends Exception {
+
+    public BusinessException() {
+    }
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BusinessException(Throwable cause) {
+        super(cause);
+    }
+
+    public BusinessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
