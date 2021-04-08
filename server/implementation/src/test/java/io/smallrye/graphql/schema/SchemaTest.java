d57f04c14142c4cccb0a2f05e51540c711c9c979
Jan Martiška <jmartisk@redhat.com>
2021-04-08 10:44:09 +0200

    Verify at bootstrap that all operations' classes are injectable

--- server/implementation/src/test/java/io/smallrye/graphql/schema/SchemaTest.java
+++ server/implementation/src/test/java/io/smallrye/graphql/schema/SchemaTest.java
@@ -12,6 +12,8 @@ import java.util.stream.Stream;
 import org.jboss.jandex.IndexView;
 import org.jboss.jandex.Indexer;
 import org.jboss.logging.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import graphql.schema.GraphQLArgument;
@@ -32,6 +34,18 @@ class SchemaTest {
             return true;
         }
     };
+
+    @BeforeEach
+    public void skipInjectionValidation() {
+        // in a unit test we don't have injection available, this is a hack needed to tell the Bootstrap class
+        // that it should not verify injection availability
+        System.setProperty("test.skip.injection.validation", "true");
+    }
+
+    @AfterEach
+    public void skipInjectionValidationCleanup() {
+        System.clearProperty("test.skip.injection.validation");
+    }
 
     @Test
     void testSchemaWithDirective() {
