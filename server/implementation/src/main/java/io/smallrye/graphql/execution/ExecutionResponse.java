a48726fd12cd2b051533a80d89907278f504a558
Yannick da Silva Bröker <ybroeker@techfak.uni-bielefeld.de>
2021-05-07 20:27:02 +0200

    Build json 'by hand'
    Formatting json with JsonB und parse afterwards was slow, formatting by hand should be much faster.
    GraphQL returns a limited set of values (Collections, Maps, Numbers, Booleans, Enums), so
    building the json by hand is easy. JsonB is used just in case if an different type is encountered.

--- server/implementation/src/main/java/io/smallrye/graphql/execution/ExecutionResponse.java
+++ server/implementation/src/main/java/io/smallrye/graphql/execution/ExecutionResponse.java
@@ -1,20 +1,15 @@
 package io.smallrye.graphql.execution;
 
 import java.io.StringReader;
-import java.util.List;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.*;
 
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonBuilderFactory;
-import javax.json.JsonObject;
-import javax.json.JsonObjectBuilder;
-import javax.json.JsonReader;
-import javax.json.JsonReaderFactory;
-import javax.json.JsonValue;
+import javax.json.*;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.json.bind.JsonbConfig;
+import javax.json.spi.JsonProvider;
 
 import graphql.ExecutionResult;
 import graphql.GraphQLError;
@@ -22,10 +17,12 @@ import io.smallrye.graphql.execution.error.ExecutionErrorsService;
 
 /**
  * Response from an execution
- * 
+ *
  * @author Phillip Kruger (phillip.kruger@redhat.com)
  */
 public class ExecutionResponse {
+
+    private static final JsonProvider jsonProvider = JsonProvider.provider();
     private static final JsonBuilderFactory jsonObjectFactory = Json.createBuilderFactory(null);
     private static final JsonReaderFactory jsonReaderFactory = Json.createReaderFactory(null);
     private static final Jsonb jsonB = JsonbBuilder.create(new JsonbConfig()
@@ -110,11 +107,66 @@ public class ExecutionResponse {
         return extensionsBuilder.build();
     }
 
+    /**
+     * Build a JsonValue from the provided Object.
+     * <p>
+     * </p>
+     * GraphQL returns a limited set of values ({@code Collection}, {@code Map}, {@code Number}, {@code Boolean}, {@code Enum}),
+     * so the json value is build by hand.
+     * Additionally, {@code JsonB} is used as a fallback if an different type is encountered.
+     * 
+     * @param pojo a java object, limited to {@code Collection}, {@code Map}, {@code Number}, {@code Boolean} and {@code Enum}
+     * @return the json value
+     */
     private JsonValue toJsonValue(Object pojo) {
-        String json = jsonB.toJson(pojo);
-        try (StringReader sr = new StringReader(json); JsonReader reader = jsonReaderFactory.createReader(sr)) {
-            return reader.readValue();
+        final JsonValue jsonValue;
+        if (pojo == null) {
+            return JsonValue.NULL;
+        } else if (pojo instanceof Map) {
+            JsonObjectBuilder jsonObjectBuilder = jsonObjectFactory.createObjectBuilder();
+            Map<String, Object> map = (Map<String, Object>) pojo;
+            for (final Map.Entry<String, Object> stringObjectEntry : map.entrySet()) {
+                jsonObjectBuilder.add(stringObjectEntry.getKey(), toJsonValue(stringObjectEntry.getValue()));
+            }
+            jsonValue = jsonObjectBuilder.build();
+        } else if (pojo instanceof Collection) {
+            Collection<Object> map = ((Collection<Object>) pojo);
+            JsonArrayBuilder builder = jsonObjectFactory.createArrayBuilder();
+            for (final Object o : map) {
+                builder.add(toJsonValue(o));
+            }
+            jsonValue = builder.build();
+        } else if (pojo instanceof Boolean) {
+            if (((Boolean) pojo)) {
+                jsonValue = JsonValue.TRUE;
+            } else {
+                jsonValue = JsonValue.FALSE;
+            }
+        } else if (pojo instanceof String) {
+            jsonValue = jsonProvider.createValue(((String) pojo));
+        } else if (pojo instanceof Double) {
+            jsonValue = jsonProvider.createValue(((Number) pojo).doubleValue());
+        } else if (pojo instanceof Float) {
+            //upcast to double would lead to precision loss
+            jsonValue = jsonProvider.createValue(new BigDecimal(String.valueOf(((Number) pojo).floatValue())));
+        } else if (pojo instanceof Long) {
+            jsonValue = jsonProvider.createValue(((Long) pojo));
+        } else if (pojo instanceof Integer || pojo instanceof Short || pojo instanceof Byte) {
+            jsonValue = jsonProvider.createValue(((Number) pojo).intValue());
+        } else if (pojo instanceof BigDecimal) {
+            jsonValue = jsonProvider.createValue(((BigDecimal) pojo));
+        } else if (pojo instanceof BigInteger) {
+            jsonValue = jsonProvider.createValue(((BigInteger) pojo));
+        } else if (pojo instanceof Enum<?>) {
+            jsonValue = jsonProvider.createValue(((Enum<?>) pojo).name());
+        } else {
+            String json = jsonB.toJson(pojo);
+            try (StringReader sr = new StringReader(json); JsonReader reader = jsonReaderFactory.createReader(sr)) {
+                jsonValue = reader.readValue();
+            }
         }
+
+        return jsonValue;
     }
 
     private static final String DATA = "data";
