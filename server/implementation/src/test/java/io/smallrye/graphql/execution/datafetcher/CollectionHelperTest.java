bb655a4e661eeb8f12f6dbdb9ae6f1e1c3ba8773
Yannick da Silva Bröker <ybroeker@techfak.uni-bielefeld.de>
2021-05-09 00:03:49 +0200

    Refactor CollectionHelperTest
    Some classes don't need to be testet, eg `singletonList(..).getClass()`, since
    those can't be used as a type. Also, checking for the exact type is not
    appropriate. Instead, it must be checked if the created collection can be
    assigned to the required type.
    
    CustomSet also appears faulty. It had no public default constructor, so a
    CustomSet can never be created and `newCollection` would have to fail. Until
    now, a hash set was intended as the expected result, but this would only
    lead to runtime errors.
    
    Signed-off-by: Yannick da Silva Bröker <ybroeker@techfak.uni-bielefeld.de>

--- server/implementation/src/test/java/io/smallrye/graphql/execution/datafetcher/CollectionHelperTest.java
+++ server/implementation/src/test/java/io/smallrye/graphql/execution/datafetcher/CollectionHelperTest.java
@@ -1,12 +1,10 @@
 package io.smallrye.graphql.execution.datafetcher;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
@@ -20,119 +18,45 @@ import java.util.Vector;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.CopyOnWriteArrayList;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CollectionHelperTest {
 
     private void test(Collection<?> c, Class<?> expected) {
         assertNotNull(c, "Return value is null");
-        assertEquals(expected, c.getClass(), "Unexpected type returned, expected " + expected + ", found: " + c.getClass());
+        assertTrue(expected.isAssignableFrom(c.getClass()),
+                "Unexpected type returned, expected subtype of " + expected + ", found: " + c.getClass());
         assertTrue(c.isEmpty(), "Unexpected non-empty collection returned: " + c);
     }
 
-    @Test
-    public void newCollection_Set() {
-        test(CollectionCreator.newCollection(Set.class.getName()), HashSet.class);
-    }
+    @ParameterizedTest
+    @ValueSource(classes = {
+            Collection.class,
 
-    @Test
-    public void newCollection_HashSet() {
-        test(CollectionCreator.newCollection(HashSet.class.getName()), HashSet.class);
-    }
+            Set.class,
+            HashSet.class,
+            LinkedHashSet.class,
+            TreeSet.class,
+            ConcurrentSkipListSet.class,
+            CustomSet.class,
 
-    @Test
-    public void newCollection_LinkedHashSet() {
-        test(CollectionCreator.newCollection(LinkedHashSet.class.getName()), LinkedHashSet.class);
-    }
+            List.class,
+            ArrayList.class,
+            LinkedList.class,
+            Stack.class,
+            Vector.class,
+            CopyOnWriteArrayList.class,
+            CustomList.class,
+    })
 
-    @Test
-    public void newCollection_TreeSet() {
-        test(CollectionCreator.newCollection(TreeSet.class.getName()), TreeSet.class);
-    }
-
-    @Test
-    public void newCollection_ConcurrentSkipListSet() {
-        test(CollectionCreator.newCollection(ConcurrentSkipListSet.class.getName()), ConcurrentSkipListSet.class);
-    }
-
-    @Test
-    public void newCollection_CustomSet() {
-        test(CollectionCreator.newCollection(CustomSet.class.getName()), HashSet.class);
-    }
-
-    @Test
-    public void newCollection_EmptySet() {
-        test(CollectionCreator.newCollection(Collections.EMPTY_SET.getClass().getName()), HashSet.class);
-    }
-
-    @Test
-    public void newCollection_EmptySetMethod() {
-        test(CollectionCreator.newCollection(Collections.emptySet().getClass().getName()), HashSet.class);
-    }
-
-    @Test
-    public void newCollection_CollectionsSingleton() {
-        test(CollectionCreator.newCollection(Collections.singleton("foo").getClass().getName()), HashSet.class);
-    }
-
-    @Test
-    public void newCollection_Collection() {
-        test(CollectionCreator.newCollection(Collection.class.getName()), ArrayList.class);
-    }
-
-    @Test
-    public void newCollection_List() {
-        test(CollectionCreator.newCollection(List.class.getName()), ArrayList.class);
-    }
-
-    @Test
-    public void newCollection_ArrayList() {
-        test(CollectionCreator.newCollection(ArrayList.class.getName()), ArrayList.class);
-    }
-
-    @Test
-    public void newCollection_LinkedList() {
-        test(CollectionCreator.newCollection(LinkedList.class.getName()), LinkedList.class);
-    }
-
-    @Test
-    public void newCollection_Stack() {
-        test(CollectionCreator.newCollection(Stack.class.getName()), Stack.class);
-    }
-
-    @Test
-    public void newCollection_Vector() {
-        test(CollectionCreator.newCollection(Vector.class.getName()), Vector.class);
-    }
-
-    @Test
-    public void newCollection_CopyOnWriteArrayList() {
-        test(CollectionCreator.newCollection(CopyOnWriteArrayList.class.getName()), CopyOnWriteArrayList.class);
-    }
-
-    @Test
-    public void newCollection_CustomList() {
-        test(CollectionCreator.newCollection(CustomList.class.getName()), CustomList.class);
-    }
-
-    @Test
-    public void newCollection_EmptyList() {
-        test(CollectionCreator.newCollection(Collections.EMPTY_LIST.getClass().getName()), ArrayList.class);
-    }
-
-    @Test
-    public void newCollection_EmptyListMethod() {
-        test(CollectionCreator.newCollection(Collections.emptyList().getClass().getName()), ArrayList.class);
-    }
-
-    @Test
-    public void newCollection_CollectionsSingletonList() {
-        test(CollectionCreator.newCollection(Collections.singletonList("foo").getClass().getName()), ArrayList.class);
+    public void newCollection(Class<? extends Collection<?>> clazz) {
+        test(CollectionCreator.newCollection(clazz.getName()), clazz);
     }
 
     static class CustomSet implements Set<Object> {
 
-        private CustomSet(String someString) {
+        public CustomSet() {
         }
 
         @Override
