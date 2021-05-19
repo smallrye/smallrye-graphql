bbe6f9134ad85c19b858b2a1335e74ac793fadfa
Yannick da Silva Bröker <ybroeker@techfak.uni-bielefeld.de>
2021-05-19 18:44:45 +0200

    Replace MethodHandles to allow building with graalvm native image 20.3
    See quarkusio/quarkus#17349

--- server/implementation/src/main/java/io/smallrye/graphql/execution/datafetcher/FieldDataFetcher.java
+++ server/implementation/src/main/java/io/smallrye/graphql/execution/datafetcher/FieldDataFetcher.java
@@ -2,8 +2,7 @@ package io.smallrye.graphql.execution.datafetcher;
 
 import static io.smallrye.graphql.SmallRyeGraphQLServerLogging.log;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
+import java.lang.reflect.InvocationTargetException;
 
 import graphql.GraphQLContext;
 import graphql.GraphQLException;
@@ -22,12 +21,11 @@ import io.smallrye.graphql.transformation.AbstractDataFetcherException;
  * intercept the result for some manipulation.
  * <p>
  *
- * @implNote method handles are used to get values,instead of plain reflection.
- *           This allows identical access to fields and methods.
- *           If the (graphql-) field has no methodName, a method handle for the (java-) field is is created.
- *           Otherwise, a method handle is created for the accessor method.
+ * @implNote If the (graphql-) field has no methodName, a {@link FieldAccessor} for the (java-) field is is created.
+ *           Otherwise, a {@link MethodAccessor} is created for the accessor method.
  *           <p>
- *           The owner is used to create the method handle independently of the source object (which could be a different
+ *           The owner is used to create the {@link PropertyAccessor} independently of the source object (which could be a
+ *           different
  *           subtype of the owner class for each call).
  */
 public class FieldDataFetcher<T> implements DataFetcher<T>, TrivialDataFetcher<T> {
@@ -42,9 +40,9 @@ public class FieldDataFetcher<T> implements DataFetcher<T>, TrivialDataFetcher<T
     private final Reference owner;
 
     /**
-     * MethodHandle to access the (java-) field or method.
+     * PropertyAccessor to access the (java-) field or method.
      */
-    private MethodHandle methodHandle;
+    private PropertyAccessor<Object> propertyAccessor;
 
     public FieldDataFetcher(final Field field, final Reference owner) {
         this.fieldHelper = new FieldHelper(field);
@@ -59,18 +57,13 @@ public class FieldDataFetcher<T> implements DataFetcher<T>, TrivialDataFetcher<T
             graphQLContext.put("context", ((SmallRyeContext) graphQLContext.get("context")).withDataFromFetcher(dfe, field));
         }
 
-        if (this.methodHandle == null) {
+        if (this.propertyAccessor == null) {
             // lazy initialize method handle, does not have to be threadsafe
-            this.methodHandle = buildMethodHandle();
+            this.propertyAccessor = buildPropertyAccessor();
         }
 
-        final Object resultFromMethodCall;
-        try {
-            Object source = dfe.getSource();
-            resultFromMethodCall = methodHandle.invoke(source);
-        } catch (Throwable throwable) {
-            throw new GraphQLException(throwable);
-        }
+        Object source = dfe.getSource();
+        Object resultFromMethodCall = propertyAccessor.get(source);
         try {
             // See if we need to transform
             @SuppressWarnings("unchecked")
@@ -88,25 +81,58 @@ public class FieldDataFetcher<T> implements DataFetcher<T>, TrivialDataFetcher<T
     }
 
     /**
-     * Creates an method handle for the (graphql-) field, using the (java-) field or method.
+     * Creates an {@link PropertyAccessor} for the (graphql-) field, using the (java-) field or method.
      * <p>
      * Accessibility (e.g. {@link java.lang.reflect.Field#canAccess(Object)}}) and types (return tapes and parameters) aren't
-     * checked,
-     * since the schema builder should only allow public fields and methods.
+     * checked, since the schema builder should only allow public fields and methods.
      *
-     * @return a method handle to access this property
+     * @return a PropertyAccessor to access this property
      */
-    private MethodHandle buildMethodHandle() {
+    private PropertyAccessor<Object> buildPropertyAccessor() {
         try {
             final Class<?> aClass = ClassloadingService.get().loadClass(owner.getClassName());
-            final MethodHandles.Lookup lookup = MethodHandles.lookup();
             if (this.field.getMethodName() == null) {
-                return lookup.unreflectGetter(aClass.getField(this.field.getPropertyName()));
+                return new FieldAccessor<>(aClass.getField(this.field.getPropertyName()));
             }
-            return lookup.unreflect(aClass.getMethod(this.field.getMethodName()));
-        } catch (IllegalAccessException | NoSuchFieldException | NoSuchMethodException e) {
+            return new MethodAccessor<>(aClass.getMethod(this.field.getMethodName()));
+        } catch (ReflectiveOperationException e) {
             throw new GraphQLException(e);
         }
     }
 
+    interface PropertyAccessor<T> {
+        T get(Object source) throws ReflectiveOperationException;
+    }
+
+    static class FieldAccessor<T> implements PropertyAccessor<T> {
+
+        private final java.lang.reflect.Field field;
+
+        FieldAccessor(final java.lang.reflect.Field field) {
+            this.field = field;
+        }
+
+        @Override
+        public T get(final Object source) throws IllegalAccessException {
+            @SuppressWarnings("unchecked")
+            T result = (T) field.get(source);
+            return result;
+        }
+    }
+
+    static class MethodAccessor<T> implements PropertyAccessor<T> {
+
+        private final java.lang.reflect.Method method;
+
+        MethodAccessor(final java.lang.reflect.Method method) {
+            this.method = method;
+        }
+
+        @Override
+        public T get(final Object source) throws InvocationTargetException, IllegalAccessException {
+            @SuppressWarnings("unchecked")
+            final T result = (T) method.invoke(source);
+            return result;
+        }
+    }
 }
