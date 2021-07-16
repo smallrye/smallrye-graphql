6f08137284cbbb876ff03417bbfda730d1bf9509
Jan Martiška <jmartisk@redhat.com>
2021-07-16 12:33:08 +0200

    Fix tracing

--- server/implementation-cdi/src/main/java/io/smallrye/graphql/cdi/tracing/TracingService.java
+++ server/implementation-cdi/src/main/java/io/smallrye/graphql/cdi/tracing/TracingService.java
@@ -18,6 +18,7 @@ import graphql.schema.GraphQLList;
 import graphql.schema.GraphQLNamedType;
 import graphql.schema.GraphQLNonNull;
 import graphql.schema.GraphQLType;
+import io.opentracing.Scope;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.smallrye.graphql.api.Context;
@@ -27,19 +28,31 @@ import io.smallrye.graphql.spi.EventingService;
 /**
  * Listening for event and create traces from it
  *
+ * FIXME: currently, this places the work of all fetchers inside an operation (execution)
+ * under the same parent, which is the execution itself. It would be cool
+ * to define some reasonable hierarchy between fetchers, so
+ * for example, when evaluating a source method requires evaluating another source method,
+ * the second one would be a child of the first one.
+ *
  * @author Jan Martiska (jmartisk@redhat.com)
  * @author Phillip Kruger (phillip.kruger@redhat.com)
  */
 public class TracingService implements EventingService {
 
     private final String SPAN_CLASS = "io.opentracing.Span";
+    private final String SCOPE_CLASS = "io.opentracing.Scope";
+
+    // key = execution ID
+    // value = the root span of this execution
     private final Map<String, Span> spans = Collections.synchronizedMap(new IdentityHashMap<>());
+    private final Map<String, Scope> scopes = Collections.synchronizedMap(new IdentityHashMap<>());
 
     private Tracer tracer;
 
     @Override
     public void beforeExecute(Context context) {
         ExecutionInput executionInput = context.unwrap(ExecutionInput.class);
+        // FIXME: if operationName is not set in the request explicitly, this is empty
         String operationName = getOperationName(executionInput);
 
         Span span = getTracer().buildSpan(operationName)
@@ -48,39 +61,17 @@ public class TracingService implements EventingService {
                 .withTag("graphql.operationType", getOperationNameString(context.getRequestedOperationTypes()))
                 .withTag("graphql.operationName", context.getOperationName().orElse(EMPTY))
                 .start();
-        tracer.activateSpan(span);
-
-        ((GraphQLContext) executionInput.getContext()).put(SPAN_CLASS, span); // TODO: Do we need this ?
-    }
-
-    @Override
-    public void beforeDataFetch(Context context) {
-        final DataFetchingEnvironment env = context.unwrap(DataFetchingEnvironment.class);
-
-        Span parentSpan = getParentSpan(getTracer(), env);
-
-        Span span = getTracer().buildSpan(getOperationName(env))
-                .asChildOf(parentSpan)
-                .withTag("graphql.executionId", context.getExecutionId())
-                .withTag("graphql.operationType", getOperationNameString(context.getOperationType()))
-                .withTag("graphql.operationName", context.getOperationName().orElse(EMPTY))
-                .withTag("graphql.parent", context.getParentTypeName().orElse(EMPTY))
-                .withTag("graphql.field", context.getFieldName())
-                .withTag("graphql.path", context.getPath())
-                .start();
-        tracer.activateSpan(span);
-
-        GraphQLContext graphQLContext = env.getContext();
-        graphQLContext.put(SPAN_CLASS, span);
-
+        Scope scope = tracer.activateSpan(span);
         spans.put(context.getExecutionId(), span);
+        scopes.put(context.getExecutionId(), scope);
     }
 
     @Override
-    public void errorDataFetch(String executionId, Throwable t) {
-        Span span = spans.get(executionId);
+    public void afterExecute(Context context) {
+        Span span = spans.remove(context.getExecutionId());
         if (span != null) {
-            logError(span, t);
+            scopes.get(context.getExecutionId()).close();
+            span.finish();
         }
     }
 
@@ -92,13 +83,49 @@ public class TracingService implements EventingService {
             error.put("event.object", t);
             error.put("event", "error");
             span.log(error);
+            span.finish();
         }
     }
 
     @Override
+    public void beforeDataFetch(Context context) {
+        final DataFetchingEnvironment env = context.unwrap(DataFetchingEnvironment.class);
+        Span parentSpan = getParentSpan(getTracer(), env);
+
+        Span span = getTracer().buildSpan(getOperationName(env))
+                .asChildOf(parentSpan)
+                .withTag("graphql.executionId", context.getExecutionId())
+                .withTag("graphql.operationType", getOperationNameString(context.getOperationType()))
+                .withTag("graphql.operationName", context.getOperationName().orElse(EMPTY))
+                .withTag("graphql.parent", context.getParentTypeName().orElse(EMPTY))
+                .withTag("graphql.field", context.getFieldName())
+                .withTag("graphql.path", context.getPath())
+                .start();
+        Scope scope = tracer.activateSpan(span);
+
+        GraphQLContext graphQLContext = env.getContext();
+        graphQLContext.put(SPAN_CLASS, parentSpan);
+        graphQLContext.put(SCOPE_CLASS, scope);
+    }
+
+    // FIXME: is the fetcher is asynchronous, this typically ends its span before
+    // the work is actually done - after the fetcher itself returns a future.
+    // We currently don't have a way to find out when
+    // the right moment to close this span is
+    @Override
     public void afterDataFetch(Context context) {
-        Span span = spans.remove(context.getExecutionId());
+        Span span = tracer.activeSpan();
+        Scope scope = ((GraphQLContext) context.unwrap(DataFetchingEnvironment.class).getContext()).get(SCOPE_CLASS);
+        scope.close();
         span.finish();
+    }
+
+    @Override
+    public void errorDataFetch(String executionId, Throwable t) {
+        Span span = spans.get(executionId);
+        if (span != null) {
+            logError(span, t);
+        }
     }
 
     @Override
@@ -115,13 +142,13 @@ public class TracingService implements EventingService {
 
     private Span getParentSpan(Tracer tracer, final DataFetchingEnvironment env) {
         final GraphQLContext localContext = env.getLocalContext();
-        if (localContext != null && localContext.hasKey(Span.class)) {
-            return localContext.get(Span.class);
+        if (localContext != null && localContext.hasKey(SPAN_CLASS)) {
+            return localContext.get(SPAN_CLASS);
         }
 
         final GraphQLContext rootContext = env.getContext();
-        if (rootContext != null && rootContext.hasKey(Span.class)) {
-            return rootContext.get(Span.class);
+        if (rootContext != null && rootContext.hasKey(SPAN_CLASS)) {
+            return rootContext.get(SPAN_CLASS);
         }
 
         return tracer.activeSpan();
