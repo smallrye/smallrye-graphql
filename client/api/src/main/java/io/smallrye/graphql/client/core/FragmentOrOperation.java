c2139b22e27728a94312233c4672c707022a69ac
Jan Martiška <jmartisk@redhat.com>
2021-07-27 12:00:41 +0200

    Some more javadoc comments

--- client/api/src/main/java/io/smallrye/graphql/client/core/FragmentOrOperation.java
+++ client/api/src/main/java/io/smallrye/graphql/client/core/FragmentOrOperation.java
@@ -1,4 +1,9 @@
 package io.smallrye.graphql.client.core;
 
+/**
+ * Represents a node in a GraphQL document that can contain either an operation (query/mutation/subscription),
+ * or a definition of a named fragment. On the top level, a GraphQL document basically consists of a list
+ * of these nodes.
+ */
 public interface FragmentOrOperation extends Buildable {
 }
