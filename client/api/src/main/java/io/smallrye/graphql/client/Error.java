d52f2fa1bad220daf3d6b855b2e662eb2325a143
Jan Martiška <jmartisk@redhat.com>
2021-08-31 10:55:13 +0200

    Add some javadoc for the Error interface

--- client/api/src/main/java/io/smallrye/graphql/client/Error.java
+++ client/api/src/main/java/io/smallrye/graphql/client/Error.java
@@ -7,12 +7,26 @@ import javax.json.JsonValue;
 
 public interface Error {
 
+    /**
+     * Textual description of the error, supplied by the service.
+     */
     String getMessage();
 
+    /**
+     * List of locations in the request that correspond to this error. Each location is a map that
+     * should contain the `line` and `column` items.
+     */
     List<Map<String, Integer>> getLocations();
 
+    /**
+     * The path to the response field where the error arose. This is represented as an array, where each item is either
+     * a string (when it represents a field), or an integer (when it represents an index).
+     */
     Object[] getPath();
 
+    /**
+     * Contents of the `extensions` field. This is used by GraphQL services to pass extra information about the error.
+     */
     Map<String, Object> getExtensions();
 
     /**
