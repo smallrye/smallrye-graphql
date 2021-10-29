92bdc8ed90ab72dc806a069e1f9d854648d7eb54
Rüdiger zu Dohna <snackbox@sinntr.eu>
2021-10-29 16:42:38 +0200

    #521: implement federation data fetcher and type resolver

--- server/implementation/src/main/java/io/smallrye/graphql/bootstrap/Bootstrap.java
+++ server/implementation/src/main/java/io/smallrye/graphql/bootstrap/Bootstrap.java
@@ -18,8 +18,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.json.Json;
@@ -28,8 +26,6 @@ import javax.json.JsonReaderFactory;
 import javax.json.bind.Jsonb;
 
 import com.apollographql.federation.graphqljava.Federation;
-import com.apollographql.federation.graphqljava._Entity;
-
 import graphql.introspection.Introspection.DirectiveLocation;
 import graphql.schema.DataFetcher;
 import graphql.schema.FieldCoordinates;
@@ -144,10 +140,10 @@ public class Bootstrap {
         // This crazy stream operation basically collects all class names where we need to verify that
         // it belongs to an injectable bean
         Stream.of(
-                schema.getQueries().stream().map(Operation::getClassName),
-                schema.getMutations().stream().map(Operation::getClassName),
-                schema.getGroupedQueries().values().stream().flatMap(Collection::stream).map(Operation::getClassName),
-                schema.getGroupedMutations().values().stream().flatMap(Collection::stream).map(Operation::getClassName))
+                        schema.getQueries().stream().map(Operation::getClassName),
+                        schema.getMutations().stream().map(Operation::getClassName),
+                        schema.getGroupedQueries().values().stream().flatMap(Collection::stream).map(Operation::getClassName),
+                        schema.getGroupedMutations().values().stream().flatMap(Collection::stream).map(Operation::getClassName))
                 .flatMap(stream -> stream)
                 .distinct().forEach(beanClassName -> {
                     // verify that the bean is injectable
@@ -191,10 +187,9 @@ public class Bootstrap {
         JsonInputRegistry.override(overrides);
 
         if (Config.get().isFederationEnabled()) {
-            this.graphQLSchema = Federation.transform(schemaBuilder.build())
-                    .fetchEntities(env -> env.<List<Map<String, Object>>> getArgument(_Entity.argumentName).stream()
-                            .map(fetchEntities())
-                            .collect(Collectors.toList()))
+            GraphQLSchema rawSchema = schemaBuilder.build();
+            this.graphQLSchema = Federation.transform(rawSchema)
+                    .fetchEntities(new FederationDataFetcher(rawSchema.getQueryType(), rawSchema.getCodeRegistry()))
                     .resolveEntityType(fetchEntityType())
                     .build();
         } else {
@@ -202,12 +197,18 @@ public class Bootstrap {
         }
     }
 
-    private Function<Map<String, Object>, ?> fetchEntities() {
-        return reference -> null; // TODO federation: implement fetcher
-    }
-
     private TypeResolver fetchEntityType() {
-        return env -> null; // TODO federation: implement type resolver
+        return env -> {
+            Object src = env.getObject();
+            if (src == null) {
+                return null;
+            }
+            GraphQLObjectType result = env.getSchema().getObjectType(src.getClass().getSimpleName()); // TODO respect @Name, etc.
+            if (result == null) {
+                throw new RuntimeException("can't resolve federated entity type " + src.getClass().getName());
+            }
+            return result;
+        };
     }
 
     private void createGraphQLDirectiveTypes() {
@@ -295,7 +296,7 @@ public class Bootstrap {
     }
 
     private void addRootObject(GraphQLObjectType.Builder rootBuilder, Set<Operation> operations,
-            String rootName) {
+                               String rootName) {
 
         for (Operation operation : operations) {
             operation = eventEmitter.fireCreateOperation(operation);
@@ -306,7 +307,7 @@ public class Bootstrap {
     }
 
     private void addGroupedRootObject(GraphQLObjectType.Builder rootBuilder,
-            Map<Group, Set<Operation>> operationMap, String rootName) {
+                                      Map<Group, Set<Operation>> operationMap, String rootName) {
         Set<Map.Entry<Group, Set<Operation>>> operationsSet = operationMap.entrySet();
 
         for (Map.Entry<Group, Set<Operation>> operationsEntry : operationsSet) {
@@ -606,7 +607,7 @@ public class Bootstrap {
     }
 
     private GraphQLFieldDefinition createGraphQLFieldDefinitionFromBatchOperation(String operationTypeName,
-            Operation operation) {
+                                                                                  Operation operation) {
         // Fields
         GraphQLFieldDefinition.Builder fieldBuilder = GraphQLFieldDefinition.newFieldDefinition()
                 .name(operation.getName())
@@ -790,7 +791,8 @@ public class Bootstrap {
                 graphQLInputType = GraphQLNonNull.nonNull(graphQLInputType);
             }
             // Collection depth
-            do {
+            do
+            {
                 if (wrapper.isCollectionOrArrayOrMap()) {
                     graphQLInputType = list(graphQLInputType);
                     wrapper = wrapper.getWrapper();
@@ -820,7 +822,8 @@ public class Bootstrap {
                 graphQLOutputType = GraphQLNonNull.nonNull(graphQLOutputType);
             }
             // Collection depth
-            do {
+            do
+            {
                 if (wrapper.isCollectionOrArrayOrMap()) {
                     graphQLOutputType = list(graphQLOutputType);
                     wrapper = wrapper.getWrapper();
@@ -920,7 +923,8 @@ public class Bootstrap {
                 graphQLInputType = GraphQLNonNull.nonNull(graphQLInputType);
             }
             // Collection depth
-            do {
+            do
+            {
                 if (wrapper.isCollectionOrArrayOrMap()) {
                     graphQLInputType = list(graphQLInputType);
                     wrapper = wrapper.getWrapper();
@@ -992,7 +996,7 @@ public class Bootstrap {
     private boolean isJsonString(String string) {
         if (string != null && !string.isEmpty() && (string.contains("{") || string.contains("["))) {
             try (StringReader stringReader = new StringReader(string);
-                    JsonReader jsonReader = jsonReaderFactory.createReader(stringReader)) {
+                 JsonReader jsonReader = jsonReaderFactory.createReader(stringReader)) {
 
                 jsonReader.readValue();
                 return true;
