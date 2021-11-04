e5cc016753a192ba150cfc5c1229eb2638b07c9e
Jan Martiška <jmartisk@redhat.com>
2021-11-04 08:05:16 +0100

    Move enum validation logic to happen earlier

--- client/implementation/src/main/java/io/smallrye/graphql/client/dynamic/core/EnumImpl.java
+++ client/implementation/src/main/java/io/smallrye/graphql/client/dynamic/core/EnumImpl.java
@@ -1,26 +1,10 @@
 package io.smallrye.graphql.client.dynamic.core;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
-
-import io.smallrye.graphql.client.core.exceptions.BuildException;
-
 public class EnumImpl extends AbstractEnum {
-
-    // According to http://spec.graphql.org/June2018/#sec-Enum-Value
-    private static final Pattern VALID_ENUM_NAME = Pattern.compile("[_A-Za-z][_0-9A-Za-z]*");
-    private static final List<String> FORBIDDEN_ENUM_NAMES = Arrays.asList(
-            "true", "false", "null");
 
     @Override
     public String build() {
-        if (!VALID_ENUM_NAME.matcher(this.getValue()).matches()) {
-            throw new BuildException("Enum value '" + this.getValue() + "' is not valid gql name");
-        }
-        if (FORBIDDEN_ENUM_NAMES.contains(this.getValue())) {
-            throw new BuildException("Enum is a forbidden name '" + this.getValue() + "'");
-        }
+        validateValue(this.getValue());
         return this.getValue();
     }
 
