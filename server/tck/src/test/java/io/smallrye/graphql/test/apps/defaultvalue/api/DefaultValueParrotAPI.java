26459710dacb44568343874e9440d62a9eb24dfe
Tuomas Kareinen <tkareine@gmail.com>
2021-11-05 17:36:38 +0200

    Support default values for collection/array of input types

--- server/tck/src/test/java/io/smallrye/graphql/test/apps/defaultvalue/api/DefaultValueParrotAPI.java
+++ server/tck/src/test/java/io/smallrye/graphql/test/apps/defaultvalue/api/DefaultValueParrotAPI.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.eclipse.microprofile.graphql.DefaultValue;
 import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Input;
+import org.eclipse.microprofile.graphql.NonNull;
 import org.eclipse.microprofile.graphql.Query;
 
 /**
  * Testing Default values
- * 
+ *
  * @author Phillip Kruger (phillip.kruger@redhat.com)
  */
 @GraphQLApi
@@ -22,5 +24,30 @@ public class DefaultValueParrotAPI {
     @Query
     public List<String> listDefault(@DefaultValue("[\"electric\",\"blue\"]") List<String> values) {
         return values;
+    }
+
+    @Query
+    public Root objectFieldDefaults(@NonNull Root input) {
+        return input;
+    }
+
+    @Input
+    public static class Root {
+        @DefaultValue("[\"dancing\",\"shepard\"]")
+        public String[] stringArray;
+
+        @DefaultValue("[\"poignant\",\"joker\"]")
+        public List<String> stringList;
+
+        @DefaultValue("[{\"field\": \"angry\"}, {\"field\": \"jack\"}]")
+        public Nested[] nestedArray;
+
+        @DefaultValue("[{\"field\": \"big\"}, {\"field\": \"grunt\"}]")
+        public List<Nested> nestedList;
+    }
+
+    @Input
+    public static class Nested {
+        public String field;
     }
 }
