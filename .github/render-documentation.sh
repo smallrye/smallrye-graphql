75564ac39718b7aa70045d381b2377e1721df568
Jan Martiška <jmartisk@redhat.com>
2021-12-06 13:44:04 +0100

    Fix documentation build

--- .github/render-documentation.sh
+++ .github/render-documentation.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-SR_GRAPHQL_VERSION=$(cat project.yml | yq eval '.release.current-version' -)  && mike deploy --push --update-aliases $SR_GRAPHQL_VERSION latest --branch gh-pages
-SR_GRAPHQL_VERSION=$(cat project.yml | yq eval '.release.current-version' -)  && mike set-default --push latest
+SR_GRAPHQL_VERSION=$(cat .github/project.yml | yq eval '.release.current-version' -)  && mike deploy --push --update-aliases $SR_GRAPHQL_VERSION latest --branch gh-pages
+SR_GRAPHQL_VERSION=$(cat .github/project.yml | yq eval '.release.current-version' -)  && mike set-default --push latest
