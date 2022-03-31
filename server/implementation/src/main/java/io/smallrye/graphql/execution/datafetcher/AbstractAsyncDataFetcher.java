a9fbed3e534ce9097dc747da62d5777ea3f90809
Phillip Kruger <phillip.kruger@gmail.com>
2022-03-31 15:32:56 +1100

    Consolidate Mutiny and CF code. Now all use Mutiny
    Signed-off-by: Phillip Kruger <phillip.kruger@gmail.com>

---
+++ server/implementation/src/main/java/io/smallrye/graphql/execution/datafetcher/AbstractAsyncDataFetcher.java
@@ -0,0 +1,100 @@
+package io.smallrye.graphql.execution.datafetcher;
+
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+
+import org.dataloader.BatchLoaderEnvironment;
+import org.eclipse.microprofile.graphql.GraphQLException;
+
+import graphql.execution.DataFetcherResult;
+import graphql.schema.DataFetchingEnvironment;
+import io.smallrye.graphql.SmallRyeGraphQLServerMessages;
+import io.smallrye.graphql.execution.context.SmallRyeContext;
+import io.smallrye.graphql.schema.model.Operation;
+import io.smallrye.graphql.transformation.AbstractDataFetcherException;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+
+/**
+ * Handle Async calls with Uni, base for Uni and CompletionState Datafetchers
+ *
+ * @param <K>
+ * @param <T>
+ * @author Phillip Kruger (phillip.kruger@redhat.com)
+ */
+public abstract class AbstractAsyncDataFetcher<K, T> extends AbstractDataFetcher<K, T> {
+
+    public AbstractAsyncDataFetcher(Operation operation) {
+        super(operation);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected <O> O invokeAndTransform(
+            DataFetchingEnvironment dfe,
+            DataFetcherResult.Builder<Object> resultBuilder,
+            Object[] transformedArguments) throws Exception {
+        SmallRyeContext context = contextHelper.getSmallRyeContext(dfe);
+        try {
+            SmallRyeContext.setContext(context);
+            Uni<?> uni = handleUserMethodCall(transformedArguments);
+            return (O) uni
+                    .onItemOrFailure()
+                    .transformToUni((result, throwable, emitter) -> {
+                        if (throwable != null) {
+                            eventEmitter.fireOnDataFetchError(dfe.getExecutionId().toString(), throwable);
+                            if (throwable instanceof GraphQLException) {
+                                GraphQLException graphQLException = (GraphQLException) throwable;
+                                errorResultHelper.appendPartialResult(resultBuilder, dfe, graphQLException);
+                            } else if (throwable instanceof Exception) {
+                                emitter.fail(SmallRyeGraphQLServerMessages.msg.dataFetcherException(operation, throwable));
+                                return;
+                            } else if (throwable instanceof Error) {
+                                emitter.fail(throwable);
+                                return;
+                            }
+                        } else {
+                            try {
+                                resultBuilder.data(fieldHelper.transformOrAdaptResponse(result, dfe));
+                            } catch (AbstractDataFetcherException te) {
+                                te.appendDataFetcherResult(resultBuilder, dfe);
+                            }
+                        }
+
+                        emitter.complete(resultBuilder.build());
+                    })
+                    .runSubscriptionOn(Infrastructure.getDefaultExecutor())
+                    .subscribe()
+                    .asCompletionStage();
+        } finally {
+            SmallRyeContext.remove();
+        }
+    }
+
+    protected abstract Uni<?> handleUserMethodCall(Object[] transformedArguments) throws Exception;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected <O> O invokeFailure(DataFetcherResult.Builder<Object> resultBuilder) {
+        return (O) Uni.createFrom()
+                .item(resultBuilder::build)
+                .runSubscriptionOn(Infrastructure.getDefaultExecutor())
+                .subscribe()
+                .asCompletionStage();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public CompletionStage<List<T>> load(List<K> keys, BatchLoaderEnvironment ble) {
+        try {
+            Object[] arguments = batchLoaderHelper.getArguments(keys, ble);
+            return handleUserBatchLoad(arguments)
+                    .runSubscriptionOn(Infrastructure.getDefaultExecutor())
+                    .subscribe().asCompletionStage();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    protected abstract Uni<List<T>> handleUserBatchLoad(final Object[] arguments) throws Exception;
+}
---
+++ server/implementation/src/main/java/io/smallrye/graphql/execution/datafetcher/AbstractStreamingDataFetcher.java
@@ -0,0 +1,102 @@
+package io.smallrye.graphql.execution.datafetcher;
+
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+
+import org.dataloader.BatchLoaderEnvironment;
+import org.eclipse.microprofile.graphql.GraphQLException;
+
+import graphql.execution.DataFetcherResult;
+import graphql.schema.DataFetchingEnvironment;
+import io.smallrye.graphql.SmallRyeGraphQLServerMessages;
+import io.smallrye.graphql.execution.context.SmallRyeContext;
+import io.smallrye.graphql.schema.model.Operation;
+import io.smallrye.graphql.transformation.AbstractDataFetcherException;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
+
+/**
+ * Handle Stream calls with Multi, used as base for Multi and Publisher
+ *
+ * @param <K>
+ * @param <T>
+ * @author Phillip Kruger (phillip.kruger@redhat.com)
+ */
+public abstract class AbstractStreamingDataFetcher<K, T> extends AbstractDataFetcher<K, T> {
+
+    public AbstractStreamingDataFetcher(Operation operation) {
+        super(operation);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected <O> O invokeAndTransform(
+            DataFetchingEnvironment dfe,
+            DataFetcherResult.Builder<Object> resultBuilder,
+            Object[] transformedArguments) throws Exception {
+        SmallRyeContext context = contextHelper.getSmallRyeContext(dfe);
+        try {
+            SmallRyeContext.setContext(context);
+            Multi<?> multi = handleUserMethodCall(transformedArguments);
+
+            return (O) multi
+                    .onItem().transform((t) -> {
+                        try {
+                            Object resultFromTransform = fieldHelper.transformOrAdaptResponse(t, dfe);
+                            resultBuilder.data(resultFromTransform);
+                            return (O) resultBuilder.build();
+                        } catch (AbstractDataFetcherException abstractDataFetcherException) {
+                            //Arguments or result couldn't be transformed
+                            abstractDataFetcherException.appendDataFetcherResult(resultBuilder, dfe);
+                            eventEmitter.fireOnDataFetchError(dfe.getExecutionId().toString(), abstractDataFetcherException);
+                            return (O) resultBuilder.build();
+                        }
+                    })
+                    .onFailure().recoverWithItem(new Function<Throwable, O>() {
+                        @Override
+                        public O apply(Throwable throwable) {
+                            eventEmitter.fireOnDataFetchError(dfe.getExecutionId().toString(), throwable);
+                            if (throwable instanceof GraphQLException) {
+                                GraphQLException graphQLException = (GraphQLException) throwable;
+                                errorResultHelper.appendPartialResult(resultBuilder, dfe, graphQLException);
+                            } else if (throwable instanceof Exception) {
+                                DataFetcherException dataFetcherException = SmallRyeGraphQLServerMessages.msg
+                                        .dataFetcherException(operation, throwable);
+                                errorResultHelper.appendException(resultBuilder, dfe, dataFetcherException);
+                            } else if (throwable instanceof Error) {
+                                errorResultHelper.appendException(resultBuilder, dfe, throwable);
+                            }
+                            return (O) resultBuilder.build();
+                        }
+                    });
+
+        } finally {
+            SmallRyeContext.remove();
+        }
+    }
+
+    protected abstract Multi<?> handleUserMethodCall(Object[] transformedArguments) throws Exception;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected <O> O invokeFailure(DataFetcherResult.Builder<Object> resultBuilder) {
+        return (O) Multi.createFrom()
+                .item(resultBuilder::build);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public CompletionStage<List<T>> load(List<K> keys, BatchLoaderEnvironment ble) {
+        try {
+            Object[] arguments = batchLoaderHelper.getArguments(keys, ble);
+            return handleUserBatchLoad(arguments)
+                    .toUni().runSubscriptionOn(Infrastructure.getDefaultExecutor())
+                    .subscribe().asCompletionStage();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    protected abstract Multi<List<T>> handleUserBatchLoad(final Object[] arguments) throws Exception;
+}
