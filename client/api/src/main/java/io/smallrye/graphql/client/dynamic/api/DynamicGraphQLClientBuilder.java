3d92418395bfb8ccab747a09d3095286d303eb69
Jan Martiška <wraychus@gmail.com>
2022-05-16 09:08:08 +0200

    Improve exception message

--- client/api/src/main/java/io/smallrye/graphql/client/dynamic/api/DynamicGraphQLClientBuilder.java
+++ client/api/src/main/java/io/smallrye/graphql/client/dynamic/api/DynamicGraphQLClientBuilder.java
@@ -32,7 +32,9 @@ public interface DynamicGraphQLClientBuilder {
         ServiceLoader<DynamicGraphQLClientBuilder> loader = ServiceLoader.load(DynamicGraphQLClientBuilder.class);
         Iterator<DynamicGraphQLClientBuilder> iterator = loader.iterator();
         if (!iterator.hasNext())
-            throw new ServiceConfigurationError("no " + DynamicGraphQLClientBuilder.class.getName() + " in classpath");
+            throw new ServiceConfigurationError(
+                    "no " + DynamicGraphQLClientBuilder.class.getName() + " implementation found via ServiceLoader," +
+                            "do you have a smallrye-graphql-client-implementation-* artifact in classpath?");
         DynamicGraphQLClientBuilder graphQlClientBuilder = iterator.next();
         if (iterator.hasNext())
             throw new ServiceConfigurationError(
