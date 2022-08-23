da4fe430f96a108c6241fbf1ebf4d218c23b62ed
Jan Martiska <jmartisk@redhat.com>
2022-08-23 08:39:03 +0200

    Split gradle tests into separate isolated test cases

--- tools/gradle-plugin/src/test/java/io/smallrye/graphql/gradle/test/GradlePluginGenerateSchemaIncludeDirectivesTest.java
+++ tools/gradle-plugin/src/test/java/io/smallrye/graphql/gradle/test/GradlePluginGenerateSchemaIncludeDirectivesTest.java
@@ -1,7 +1,6 @@
 package io.smallrye.graphql.gradle.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -19,7 +18,7 @@ import org.gradle.testkit.runner.TaskOutcome;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class GradlePluginGenerateSchemaTest {
+public class GradlePluginGenerateSchemaIncludeDirectivesTest {
 
     private static final File PROJECT_DIR = new File("testing-project");
     private static final File SCHEMA_FILE = new File("testing-project/build/generated/schema.graphql");
@@ -30,13 +29,9 @@ public class GradlePluginGenerateSchemaTest {
     }
 
     @Test
-    public void testDefaults() throws IOException {
-        String schema = execute(Collections.emptyList());
-        assertTrue(schema.contains("type Query {"));
-        assertFalse(schema.contains("type org_acme_Foo"), "Short class names should be used for GraphQL types");
-        assertFalse(schema.contains("directive @skip"), "Directives should not be included");
-        assertFalse(schema.contains("type __Schema"), "Introspection types should not be included");
-        assertFalse(schema.contains("schema {"), "Schema definition should not be included");
+    public void testIncludeDirectives() throws IOException {
+        String schema = execute(Collections.singletonList("-DincludeDirectives=true"));
+        assertTrue(schema.contains("directive @skip"), "Directives should be included: " + schema);
     }
 
     private String execute(List<String> arguments) throws IOException {
