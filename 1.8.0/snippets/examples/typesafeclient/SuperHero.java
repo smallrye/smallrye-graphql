6283a6d5916bfdbbd78edea2ca0abebe4c978909
SmallRye CI <smallrye@googlegroups.com>
2022-09-14 09:52:17 +0000

    Deployed f600906f to 1.8.0 with MkDocs 1.3.1 and mike 1.1.2

---
+++ 1.8.0/snippets/examples/typesafeclient/SuperHero.java
@@ -0,0 +1,12 @@
+package examples.typesafeclient;
+
+import java.util.List;
+
+public class SuperHero {
+
+    private String name;
+    private List<String> superPowers;
+
+    // plus getters and setters
+
+}
