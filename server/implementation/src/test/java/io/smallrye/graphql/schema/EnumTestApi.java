13429007c5080751cac64ae96f47088a110d0cab
Philip Graf <git@acanda.ch>
2022-09-16 11:17:38 +0300

    Support directives on enums

---
+++ server/implementation/src/test/java/io/smallrye/graphql/schema/EnumTestApi.java
@@ -0,0 +1,20 @@
+package io.smallrye.graphql.schema;
+
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
+
+@GraphQLApi
+public class EnumTestApi {
+
+    @Query
+    public int query(EnumWithDirectives enumWithDirectives) {
+        return 0;
+    }
+
+    @EnumDirective
+    public enum EnumWithDirectives {
+        @EnumDirective
+        A
+    }
+
+}
