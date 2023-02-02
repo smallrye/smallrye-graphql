587a1069ad6628eddf5e13907af51146b8ad7b10
Erik Bakker <erik@eamelink.net>
2023-02-02 08:59:49 +0100

    Add multi-module support to Maven plugin

---
+++ tools/maven-plugin-tests/testing-project-multi-module/model/src/main/java/org/acme/model/Foo.java
@@ -0,0 +1,15 @@
+package org.acme.model;
+
+
+public class Foo {
+
+    private Integer number;
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+}
