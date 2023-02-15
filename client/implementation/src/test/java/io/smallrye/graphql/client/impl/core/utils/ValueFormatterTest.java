447320dbec4e82e032194982c34c9a41da8477cc
aliakseidudko <aliaksei.dudko@tealium.com>
2023-02-16 00:38:54 +0100

    issue-1747: Unable to pass enum as argument in GraphQL client query

--- client/implementation/src/test/java/io/smallrye/graphql/client/impl/core/utils/ValueFormatterTest.java
+++ client/implementation/src/test/java/io/smallrye/graphql/client/impl/core/utils/ValueFormatterTest.java
@@ -1,5 +1,6 @@
 package io.smallrye.graphql.client.impl.core.utils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Date;
@@ -23,6 +24,19 @@ class ValueFormatterTest {
         assertThrows(IllegalStateException.class, () -> {
             ValueFormatter.format(new Date());
         });
+    }
+
+    @Test
+    public void testEnumInput() {
+
+        String value = ValueFormatter.format(Order.ASC);
+
+        assertEquals(Order.ASC.toString(), value);
+    }
+
+    enum Order {
+        ASC,
+        DESC
     }
 
 }
