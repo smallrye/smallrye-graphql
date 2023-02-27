b7a0fcb101efb3355d5192117517d8169b5c312c
Paul Seyerlein <seyerlein@e2n.de>
2023-02-27 15:47:14 +0100

    inlined test variables

--- server/implementation-cdi/src/test/java/io/smallrye/graphql/cdi/config/MicroProfileConfigTest.java
+++ server/implementation-cdi/src/test/java/io/smallrye/graphql/cdi/config/MicroProfileConfigTest.java
@@ -14,64 +14,55 @@ class MicroProfileConfigTest {
     @Test
     void testHideErrorMessageList() {
         assertTrue(config.getHideErrorMessageList().isPresent());
-        List<String> hiddenMessages = config.getHideErrorMessageList().get();
-        assertEquals(List.of("a", "b", "c", "b", "c", "d"), hiddenMessages);
+        assertEquals(List.of("a", "b", "c", "b", "c", "d"), config.getHideErrorMessageList().get());
     }
 
     @Test
     void testShowErrorMessageList() {
         assertTrue(config.getShowErrorMessageList().isPresent());
-        List<String> shownMessages = config.getShowErrorMessageList().get();
-        assertEquals(List.of("a", "b", "c", "b", "c", "d"), shownMessages);
+        assertEquals(List.of("a", "b", "c", "b", "c", "d"), config.getShowErrorMessageList().get());
     }
 
     @Test
     void testUnwrapExceptions() {
         assertTrue(config.getUnwrapExceptions().isPresent());
-        List<String> unwrappedExceptions = config.getUnwrapExceptions().get();
-        assertEquals(List.of("a", "b", "c"), unwrappedExceptions);
+        assertEquals(List.of("a", "b", "c"), config.getUnwrapExceptions().get());
     }
 
     @Test
     void testErrorExtensionFields() {
         assertTrue(config.getErrorExtensionFields().isPresent());
-        List<String> errorExtensionFields = config.getErrorExtensionFields().get();
-        assertEquals(List.of("a", "b", "c"), errorExtensionFields);
+        assertEquals(List.of("a", "b", "c"), config.getErrorExtensionFields().get());
     }
 
     @Test
     void testQueryComplexityInstrumentation() {
         assertTrue(config.getQueryComplexityInstrumentation().isPresent());
-        Integer queryComplexityInstrumentation = config.getQueryComplexityInstrumentation().get();
-        assertEquals(1337, queryComplexityInstrumentation);
+        assertEquals(1337, config.getQueryComplexityInstrumentation().get());
     }
 
     @Test
     void testQueryDepthInstrumentation() {
         assertTrue(config.getQueryDepthInstrumentation().isPresent());
-        Integer queryDepthInstrumentation = config.getQueryDepthInstrumentation().get();
-        assertEquals(1338, queryDepthInstrumentation);
+        assertEquals(1338, config.getQueryDepthInstrumentation().get());
     }
 
     @Test
     void testParserMaxTokens() {
         assertTrue(config.getParserMaxTokens().isPresent());
-        Integer parserMaxTokens = config.getParserMaxTokens().get();
-        assertEquals(1339, parserMaxTokens);
+        assertEquals(1339, config.getParserMaxTokens().get());
     }
 
     @Test
     void testParserMaxWhitespaceTokens() {
         assertTrue(config.getParserMaxWhitespaceTokens().isPresent());
-        Integer parserMaxWhitespaceTokens = config.getParserMaxWhitespaceTokens().get();
-        assertEquals(1340, parserMaxWhitespaceTokens);
+        assertEquals(1340, config.getParserMaxWhitespaceTokens().get());
     }
 
     @Test
     void testParserCaptureSourceLocation() {
         assertTrue(config.isParserCaptureSourceLocation().isPresent());
-        Boolean isParserCaptureSourceLocation = config.isParserCaptureSourceLocation().get();
-        assertEquals(Boolean.TRUE, isParserCaptureSourceLocation);
+        assertEquals(Boolean.TRUE, config.isParserCaptureSourceLocation().get());
     }
 
     @Test
