f56bc6688230e4806989bc6a651b4f96e6d04e1a
Paul Seyerlein <seyerlein@e2n.de>
2023-02-28 11:59:09 +0100

    fixed import order (again)

--- server/implementation-cdi/src/test/java/io/smallrye/graphql/cdi/config/MicroProfileConfigTest.java
+++ server/implementation-cdi/src/test/java/io/smallrye/graphql/cdi/config/MicroProfileConfigTest.java
@@ -1,11 +1,11 @@
 package io.smallrye.graphql.cdi.config;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 class MicroProfileConfigTest {
 
