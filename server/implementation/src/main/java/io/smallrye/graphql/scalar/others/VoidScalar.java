f236d666c91dbdfbc16a86dd5366e8de482eacb0
Marek Skacelik <marek.skacelik@gmail.com>
2023-04-26 09:23:52 +0200

    Added Void (scalar) coercing

--- server/implementation/src/main/java/io/smallrye/graphql/scalar/others/VoidScalar.java
+++ server/implementation/src/main/java/io/smallrye/graphql/scalar/others/VoidScalar.java
@@ -1,9 +1,15 @@
 package io.smallrye.graphql.scalar.others;
 
-import io.smallrye.graphql.scalar.time.AbstractDateScalar;
+import graphql.schema.Coercing;
+import io.smallrye.graphql.scalar.AbstractScalar;
 
-public class VoidScalar extends AbstractDateScalar {
+public class VoidScalar extends AbstractScalar {
     public VoidScalar() {
-        super("Void", Void.class, void.class);
+        super("Void", new Coercing() {
+            @Override
+            public Object serialize(Object input) {
+                return null;
+            }
+        }, Void.class, void.class);
     }
 }
