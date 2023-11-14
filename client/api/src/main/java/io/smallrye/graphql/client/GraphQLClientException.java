bb7d10c1368e5fe01aabc588037a739ae4cc49b2
Anton Pinsky <anton.pinsky@ionos.com>
2023-11-14 09:46:12 +0100

    [#1965] Filter out the null elements from the list of errors in GraphQLClientException

--- client/api/src/main/java/io/smallrye/graphql/client/GraphQLClientException.java
+++ client/api/src/main/java/io/smallrye/graphql/client/GraphQLClientException.java
@@ -1,10 +1,12 @@
 package io.smallrye.graphql.client;
 
+import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.joining;
 
-import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Represents a response that contained application-level errors and thus can't be turned into a domain object.
@@ -17,12 +19,12 @@ public class GraphQLClientException extends RuntimeException {
 
     public GraphQLClientException(String message, GraphQLError error) {
         super(message);
-        this.errors = Collections.singletonList(requireNonNull(error));
+        this.errors = singletonList(requireNonNull(error));
     }
 
     public GraphQLClientException(String message, List<GraphQLError> errors) {
         super(message);
-        this.errors = requireNonNull(errors);
+        this.errors = requireNonNull(errors).stream().filter(Objects::nonNull).collect(Collectors.toList());
     }
 
     @Override
