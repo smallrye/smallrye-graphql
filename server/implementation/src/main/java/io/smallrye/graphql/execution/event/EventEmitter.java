232b2264f52b96bab013cafa7ac0946ddb4e4dae
Jan Martiska <jmartisk@redhat.com>
2024-01-17 09:53:02 +0100

    Reformat, change the logging a bit

--- server/implementation/src/main/java/io/smallrye/graphql/execution/event/EventEmitter.java
+++ server/implementation/src/main/java/io/smallrye/graphql/execution/event/EventEmitter.java
@@ -174,10 +174,15 @@ public class EventEmitter {
 
     public Map<String, Jsonb> fireOverrideJsonbConfig() {
         Map<String, Jsonb> overrides = new HashMap<>();
-        int counter = 1;
         for (EventingService extensionService : enabledServices) {
-            LOG.debug(counter++ + ". Registering custom config for EventingService class - " + extensionService.getClass().getName());
-            overrides.putAll(extensionService.overrideJsonbConfig());
+            Map<String, Jsonb> map = extensionService.overrideJsonbConfig();
+            map.forEach((clazz, jsonb) -> {
+                LOG.debug("Registering custom JsonB config for class " + clazz + " (it was returned by "
+                        + extensionService.getClass()
+                                .getName()
+                        + ")");
+                overrides.put(clazz, jsonb);
+            });
         }
         return overrides;
     }
