5fe6673f07e7631e751367f8b74e48a554e97188
Roman Lovakov <rlovakov@nota.tech>
2024-10-07 19:42:38 +0300

    Add autodetect federation directives in gradle plugin

--- tools/gradle-plugin/testing-project/src/main/java/org/acme/Foo.java
+++ tools/gradle-plugin/testing-project/src/main/java/org/acme/Foo.java
@@ -1,9 +1,29 @@
 package org.acme;
 
+import io.smallrye.graphql.api.federation.FieldSet;
+import io.smallrye.graphql.api.federation.Key;
 
+@Key(fields = @FieldSet("id"))
 public class Foo {
+    private Integer id;
 
     private Integer number;
+
+    public Foo() {
+    }
+
+    public Foo(Integer id, Integer number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public Integer getNumber() {
         return number;
