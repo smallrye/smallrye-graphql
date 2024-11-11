2d16939309c1c70901ec2144cef776bd3465cf1f
Marek Skacelik <marek.skacelik@gmail.com>
2024-11-11 14:44:53 +0100

    removed unnecessary "data": null if request failed before execution (via GraphQL specification)

--- server/integration-tests/src/test/java/io/smallrye/graphql/tests/error/UnparseableDocumentTestCase.java
+++ server/integration-tests/src/test/java/io/smallrye/graphql/tests/error/UnparseableDocumentTestCase.java
@@ -1,11 +1,13 @@
 package io.smallrye.graphql.tests.error;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+
 import java.net.URL;
 
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
@@ -47,7 +49,14 @@ public class UnparseableDocumentTestCase {
         String exceptionMessage = "Unparseable input document";
 
         String response = graphQLAssured.post(request);
-        MatcherAssert.assertThat(response, Matchers.containsString(exceptionMessage));
+        assertThat(response, containsString(exceptionMessage));
+        // The response should not contain the "data" field.
+        // See: https://spec.graphql.org/draft/#sec-Response-Format
+        //// If the request included execution, the response map must contain an entry with key data.
+        //// The value of this entry is described in the "Data" section.
+        //// If the request failed before execution due to a syntax error, missing information,
+        //// or validation error, this entry must not be present.
+        assertThat(response, not(containsString("\"data\":")));
     }
 
 }
