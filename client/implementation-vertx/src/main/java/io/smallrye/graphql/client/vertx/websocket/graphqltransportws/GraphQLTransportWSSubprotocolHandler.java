2bd0b8acfe4197151f75eb4ec1bfb2bd955f91b3
Luke Morfill <lukem@ziax.com>
2025-08-22 11:54:34 +0100

    Make sure that exceptions are propagated when a WebSocket fails to open

--- client/implementation-vertx/src/main/java/io/smallrye/graphql/client/vertx/websocket/graphqltransportws/GraphQLTransportWSSubprotocolHandler.java
+++ client/implementation-vertx/src/main/java/io/smallrye/graphql/client/vertx/websocket/graphqltransportws/GraphQLTransportWSSubprotocolHandler.java
@@ -100,16 +100,17 @@ public class GraphQLTransportWSSubprotocolHandler implements WebSocketSubprotoco
 
             webSocket.closeHandler((v) -> {
                 onClose.run();
+                Exception exception;
                 if (webSocket.closeStatusCode() != null) {
                     if (webSocket.closeStatusCode() == 1000) {
                         log.debug("WebSocket closed with status code 1000");
+                        exception = new UnexpectedCloseException("Connection closed before data was received", 1000);
                         // even if the status code is OK, any unfinished single-result operation
                         // should be marked as failed
-                        uniOperations.forEach((id, emitter) -> emitter.fail(
-                                new UnexpectedCloseException("Connection closed before data was received", 1000)));
+                        uniOperations.forEach((id, emitter) -> emitter.fail(exception));
                         multiOperations.forEach((id, emitter) -> emitter.complete());
                     } else {
-                        UnexpectedCloseException exception = new UnexpectedCloseException(
+                        exception = new UnexpectedCloseException(
                                 "Server closed the websocket connection with code: "
                                         + webSocket.closeStatusCode() + " and reason: " + webSocket.closeReason(),
                                 webSocket.closeStatusCode());
@@ -117,10 +118,12 @@ public class GraphQLTransportWSSubprotocolHandler implements WebSocketSubprotoco
                         multiOperations.forEach((id, emitter) -> emitter.fail(exception));
                     }
                 } else {
-                    InvalidResponseException exception = new InvalidResponseException("Connection closed");
+                    exception = new InvalidResponseException("Connection closed");
                     uniOperations.forEach((id, emitter) -> emitter.fail(exception));
                     multiOperations.forEach((id, emitter) -> emitter.fail(exception));
                 }
+
+                initializationEmitter.fail(exception);
             });
             webSocket.exceptionHandler(this::failAllActiveOperationsWith);
 
