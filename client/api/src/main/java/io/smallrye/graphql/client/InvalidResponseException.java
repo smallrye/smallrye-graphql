e20aad55a0f890988034b6c983922b4f5cecc1f6
Jan Martiska <wraychus@gmail.com>
2025-11-04 08:37:44 +0100

    Allow accessing headers from invalid responses

--- client/api/src/main/java/io/smallrye/graphql/client/InvalidResponseException.java
+++ client/api/src/main/java/io/smallrye/graphql/client/InvalidResponseException.java
@@ -1,5 +1,8 @@
 package io.smallrye.graphql.client;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Marks a response that had unexpected contents and the client was unable to properly process it.
  * This can be either due to an error on the server side (returning non-conformant responses),
@@ -8,12 +11,30 @@ package io.smallrye.graphql.client;
  */
 public class InvalidResponseException extends RuntimeException {
 
+    private final Map<String, List<String>> transportMeta;
+
     public InvalidResponseException(String message) {
         super(message);
+        this.transportMeta = null;
     }
 
     public InvalidResponseException(String message, Throwable cause) {
         super(message, cause);
+        this.transportMeta = null;
+    }
+
+    public InvalidResponseException(String message, Throwable cause, Map<String, List<String>> transportMeta) {
+        super(message, cause);
+        this.transportMeta = transportMeta;
+    }
+
+    /**
+     * Get transport-specific metadata that came from the server with the invalid response.
+     * For HTTP, these are the response headers. It can be null if headers aren't applicable, for example
+     * if this is coming from a message received over a WebSocket connection.
+     */
+    public Map<String, List<String>> getTransportMeta() {
+        return transportMeta;
     }
 
 }
