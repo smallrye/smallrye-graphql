29505931ec3c751ef772b1ca6bef89df4d36082d
Guillaume Smet <guillaume.smet@gmail.com>
2026-03-30 10:45:58 +0200

    Make Schema properties ordered
    We pass the Schema as a recorder parameter in Quarkus so we need it to
    be reproducible.
    We count on Jandex to provide a reproducible ordering and we just have
    to use data structures that preserve ordering to fix the issue.

--- common/schema-model/src/main/java/io/smallrye/graphql/schema/model/GroupContainer.java
+++ common/schema-model/src/main/java/io/smallrye/graphql/schema/model/GroupContainer.java
@@ -5,8 +5,8 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -14,8 +14,8 @@ import java.util.Set;
 public class GroupContainer implements Serializable {
     private String name;
     private String description;
-    private Set<Operation> operations = new HashSet<>();
-    private Map<String, GroupContainer> container = new HashMap<>();
+    private Set<Operation> operations = new LinkedHashSet<>();
+    private Map<String, GroupContainer> container = new LinkedHashMap<>();
 
     public GroupContainer() {
     }
